664009b0b633a417717d2becbbe22039044cac5d
Gladson Routh <grauth65@gmail.com>
2024-11-06 23:06:58 +0530

    update

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -22,7 +22,7 @@ function Sign() {
             
             <div className="w-screen h-screen flex justify-center items-center">
                 <h1 className="absolute top-10 md:top-20 text-red-500 w-screen py-2 px-20 text-center"></h1>
-                <div className="bg-yellow-200 pb-7 w-7/12 lg:w-2/5 h-9/12 lg:h-3/5 rounded-2xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] overflow-hidden">
+                <div className="bg-yellow-200 pb-7 w-9/12 lg:w-2/5 h-9/12 lg:h-3/5 rounded-2xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] overflow-hidden">
                     <h1 className="text-center mt-4 py-2 text-3xl">Log in</h1>
                     <form className="h-80 mt-7 flex flex-col justify-center gap-2 px-7 pb-4">
                         
